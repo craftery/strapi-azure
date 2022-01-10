8cf5109fcce4e0efc01fa40478a241944cd16aec
crafman <cormac.raftery@gmail.com>
2022-01-10 20:51:35 +0000

    Update server.js
    asda

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 const strapi = require('./node_modules/strapi/lib/index');
 const strapiDev=require('./node_modules/strapi/lib/commands/develop')
-
+//sdfsdfs
 if (process.env.DEV_MODE === true) {
   strapiDev();
 } else {
